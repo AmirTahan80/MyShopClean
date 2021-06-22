7083c4620ebb78030f504ffbe84e9a21c1adffb9
AMIR TAHAN <amirhosin6402@gmail.com>
2021-06-22 13:39:13 +0430

    Set Descriptions For Inter Faces In IAccountServices

--- Application/InterFaces/Admin/IAccountServices.cs
+++ Application/InterFaces/Admin/IAccountServices.cs
@@ -6,9 +6,34 @@ namespace Application.InterFaces.Admin
 {
     public interface IAccountServices
     {
+        /// <summary>
+        /// ساخت اکانت کاربری با ایمیل و رمز عبور
+        /// Create User With PassWord And Email
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="userPassword"></param>
+        /// <returns></returns>
         Task<bool> CreateUserByEmailAndPass(CreateAccountViewModel user, string userPassword);
+        /// <summary>
+        /// گرفتن تمام کاربران از دیتابیس
+        /// Get All Users From Data Base
+        /// </summary>
+        /// <returns></returns>
         Task<IList<UsersListViewModel>> GetAllUsersListAsync();
+        /// <summary>
+        /// یافتن و یا پیداکردن یک کاربر با آیدی
+        /// Find an User from Id
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         Task<UserDetailViewModel> FinUserById(string userId);
+        /// <summary>
+        /// ساخت و جنریت لینک برای تاییدیه ایمیل و ارسال به جیمیل کاربر
+        /// Generate Link And Send To User Email For Confirm Email
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
         Task<bool> ConfirmEmailAsync(string userName, string token);
     }
 }
