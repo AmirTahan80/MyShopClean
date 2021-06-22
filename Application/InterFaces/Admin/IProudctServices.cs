ebe2abb9672c737396c284aa675639ce57e61b1a
AMIR TAHAN <amirhosin6402@gmail.com>
2021-06-22 13:52:17 +0430

    Add Comments And Tip In IProductServices

--- Application/InterFaces/Admin/IProudctServices.cs
+++ Application/InterFaces/Admin/IProudctServices.cs
@@ -11,16 +11,58 @@ namespace Application.InterFaces.Admin
 {
     public interface IProudctServices
     {
+        /// <summary>
+        /// گرفتن تمامی محصولات از دیتابیس و نمایش آنها به صورت لیست
+        /// Get All Products From data Base And Show them In List
+        /// </summary>
+        /// <returns>IEnumerable<GetProductsAndImageSrcViewModel></returns>
         Task<IEnumerable<GetProductsAndImageSrcViewModel>> GetAllProductsAsync();
+        /// <summary>
+        /// گرفتن یک محصول و یا یافتن آن از طریق ای دی محصول برای نمایش و ویرایش
+        /// Get Or Find Product From Id For Show Or Edit
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns>GetProductViewModel</returns>
         Task<GetProductViewModel> GetProductAsync(int productId);
+        /// <summary>
+        /// گرفتن درختی دسته ها برای افزودن محصول
+        /// Get Categories Like Tree View For Add Product
+        /// </summary>
+        /// <returns>AddProductViewModel</returns>
         Task<AddProductViewModel> GetCategoriesTreeViewForAdd();
+        /// <summary>
+        /// حذف محصول (ها) از داخل لیست و دیتابیس 
+        /// </summary>
+        /// <param name="deleteListOfProducts"></param>
+        /// <returns>bool : True or False</returns>
         Task<bool> DeleteListOfProducts(IEnumerable<GetProductsAndImageSrcViewModel> deleteListOfProducts);
-
+        /// <summary>
+        /// ویرایش محصول 
+        /// Edit Product
+        /// </summary>
+        /// <param name="editProduct"></param>
+        /// <returns>bool : True or False</returns>
         Task<bool> EditProductAsync(GetProductViewModel editProduct);
+        /// <summary>
+        /// آپلود عکس ها از CkEditr 5 داخل پوشه و نمایش آن
+        /// Upload File From CkEdit5 to Folders And Show Them
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns>JsonResult</returns>
         JsonResult UploadFileEditor(IFormFile file);
+        /// <summary>
+        /// حذف عکس محصول به صورت تکی از دیتابیس
+        /// Delete a photo from database
+        /// </summary>
+        /// <param name="imageId"></param>
+        /// <returns>bool : True or False</returns>
         Task<bool> DeletePhotoAsync(int imageId);
-        
+        /// <summary>
+        /// افزودن محصول
+        /// Add Product
+        /// </summary>
+        /// <param name="addProduct"></param>
+        /// <returns>bool : True or False</returns>
         Task<bool> AddProductAsync(AddProductViewModel addProduct);
-        //Task<bool> DeleteProductAsync(int productId);
     }
 }
