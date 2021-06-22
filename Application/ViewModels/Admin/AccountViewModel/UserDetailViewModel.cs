b8b4dfc5c6b6fe2a2f1fa8cfdd912768e1cc5658
AMIR TAHAN <amirhosin6402@gmail.com>
2021-06-22 13:31:48 +0430

    Create UserDetailViewModel Properties

--- Application/ViewModels/Admin/AccountViewModel/UserDetailViewModel.cs
+++ Application/ViewModels/Admin/AccountViewModel/UserDetailViewModel.cs
@@ -1,7 +1,19 @@
-﻿namespace Application.ViewModels.Admin
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.ViewModels.Admin
 {
     public class UserDetailViewModel
     {
-
+        [Required]
+        public string UserId { get; set; }
+        [Required(ErrorMessage ="فیلد {0} اجباری است")]
+        [Display(Name ="نام کاربری")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage ="فیلد {0} اجباری است")]
+        [Display(Name ="ایمیل")]
+        public string UserEmail { get; set; }
+        [Required(ErrorMessage ="فیلد {0} اجباری است")]
+        [Display(Name ="مقام")]
+        public string RoleName { get; set; }
     }
 }
